e1ad98d98fac6a64fba6c4c7a6b0de304fbd184c
Mark Tullsen <tullsen@galois.com>
2022-07-19 21:11:43 -0700

    tutorial example: rename, misc

--- ouroboros-consensus-test/test-examples/Test/Example.hs
+++ ouroboros-consensus-test/test-examples/Test/Example.hs
@@ -27,13 +27,9 @@ import           Test.Tasty.QuickCheck
 import           NoThunks.Class (NoThunks, OnlyCheckWhnfNamed (..))
 
 -- pkg ouroboros-consensus:
-import           Ouroboros.Consensus.Protocol.Abstract
-import           Ouroboros.Consensus.Config.SecurityParam
-                 -- though ...Protocol.Abstract does a convenience re-export
-
-import           Ouroboros.Consensus.Block.Abstract -- (SlotNo)
+import           Ouroboros.Consensus.Block.Abstract
 import           Ouroboros.Consensus.Block.SupportsProtocol
-
+import           Ouroboros.Consensus.Protocol.Abstract
 import           Ouroboros.Consensus.Ticked
 
 
@@ -51,7 +47,7 @@ prop_example1 = True
 
 k :: SecurityParam
 k = SecurityParam {maxRollbacks= 0}
-  -- Q. any reason to put into ConsensusConfig?
+  -- Q. any reason we need to put into ConsensusConfig?
   
 data SP             -- The Simplest Protocol
 
@@ -62,13 +58,13 @@ data SP_IsLeader    = SP_IsLeader    -- Evidence that we /are/ leader
 data instance ConsensusConfig SP =
   SP_Config { cfgsp_iLeadInSlots :: Set SlotNo }
   deriving NoThunks via OnlyCheckWhnfNamed "SP_Config" (ConsensusConfig SP)
-           -- TODO: huh?
 
 instance ConsensusProtocol SP where
   type ChainDepState SP = ()
-  type LedgerView    SP = ()
   type IsLeader      SP = SP_IsLeader
   type CanBeLeader   SP = SP_CanBeLeader
+  type SelectView    SP = BlockNo
+  type LedgerView    SP = ()
   type ValidateView  SP = ()
   type ValidationErr SP = Void
 
@@ -89,67 +85,72 @@ instance ConsensusProtocol SP where
   
   reupdateChainDepState _ _ _ _ = ()
 
----- Trivial Block (to work with SP) -----------------------------------------
+
+---- Trivial Block (for the SP protocol) -------------------------------------
 -- see 4.3 in [CCASL]
+--
+-- borrowing from
+--  - https://iohk.io/en/blog/posts/2020/05/28/the-abstract-nature-of-the-consensus-layer/
+--      which references 'MiniConsensus.hs'
+--  - ouroboros-consensus-test/test-consensus/Test/Consensus/HardFork/Combinator/A.hs
 
--- borrowing from 'MiniConsensus.hs' via
--- https://iohk.io/en/blog/posts/2020/05/28/the-abstract-nature-of-the-consensus-layer/
-
-data ByronBlock =
-    ByronBlock
-      { bbSignature :: Header ByronBlock
+data TrivBlock =
+    TrivBlock
+      { bbSignature :: Header TrivBlock
       , bbEpoch     :: EpochNo
       -- , bbRelSlot   :: RelSlot
       }
-  deriving NoThunks via OnlyCheckWhnfNamed "ByronBlock" ByronBlock
+  deriving NoThunks via OnlyCheckWhnfNamed "TrivBlock" TrivBlock
 
   -- TODO: change name, this defn. too simple for Byron!
 
-type instance BlockProtocol ByronBlock = SP
+type instance BlockProtocol TrivBlock = SP
 
 
 -- BlockSupportsProtocol has *many* superclasses!
 
 
-instance BlockSupportsProtocol ByronBlock where
+instance BlockSupportsProtocol TrivBlock where
   validateView = stub -- const bbSignature
-  -- selectView   = bbSlotNo  -- has default (to ?)
+  selectView   = stub -- bbSlotNo  -- has default (to ?)
 
 
-data instance Header ByronBlock = Signature
-  deriving NoThunks via OnlyCheckWhnfNamed "ByronBlock" (Header ByronBlock)
+data instance Header TrivBlock = Signature
+  deriving NoThunks via OnlyCheckWhnfNamed "TrivBlock" (Header TrivBlock)
 
-instance GetHeader ByronBlock where
+instance GetHeader TrivBlock where
   getHeader          = bbSignature
   blockMatchesHeader = \_ _ -> True -- We are not interested in integrity here
   headerIsEBB        = const Nothing
 
-instance GetPrevHash ByronBlock where
+instance GetPrevHash TrivBlock where
   headerPrevHash = stub -- hdrB_prev
 
-instance HasHeader ByronBlock where
+instance HasHeader TrivBlock where
   getHeaderFields = stub 
                     -- see doc in *.Block.Abstract ; ~complex
                     -- getBlockHeaderFields - seems to rely on a bit more
                     
-instance StandardHash ByronBlock
+instance StandardHash TrivBlock
+  -- ?!
+  
+type instance HeaderHash  TrivBlock = String -- Strict.ByteString
 
-type instance HeaderHash  ByronBlock = String -- Strict.ByteString
-
-instance HasHeader (Header ByronBlock) where
-  getHeaderFields _b = stub :: HeaderFields (Header ByronBlock)
+instance HasHeader (Header TrivBlock) where
+  getHeaderFields _b = stub -- :: HeaderFields (Header TrivBlock)
                     -- castHeaderFields . bbSignature
 
-data instance BlockConfig ByronBlock = BCfgByronBlock
+data instance BlockConfig TrivBlock = BCfgTrivBlock
   deriving (Generic, NoThunks)
 
-data instance CodecConfig ByronBlock = CCfgByronBlock
+data instance CodecConfig TrivBlock = CCfgTrivBlock
   deriving (Generic, NoThunks)
 
-data instance StorageConfig ByronBlock = SCfgByronBlock
+data instance StorageConfig TrivBlock = SCfgTrivBlock
   deriving (Generic, NoThunks)
 
 
 ---- library -----------------------------------------------------------------
 
+stub :: a
 stub = error "stub"
